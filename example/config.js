c4a479e33b165e5003f463c09728f75867a17ae5
John Bender <john.m.bender@gmail.com>
2015-06-16 14:34:00 -0400

    whitespace

--- example/config.js
+++ example/config.js
@@ -1,53 +1,53 @@
 var path = require( "path" );
 
 module.exports = {
-	// CSS filenames
-	datasvgcss: "icons.data.svg.css",
-	datapngcss: "icons.data.png.css",
-	urlpngcss: "icons.fallback.css",
+  // CSS filenames
+  datasvgcss: "icons.data.svg.css",
+  datapngcss: "icons.data.png.css",
+  urlpngcss: "icons.fallback.css",
 
-	// preview HTML filename
-	previewhtml: "preview.html",
+  // preview HTML filename
+  previewhtml: "preview.html",
 
-	// grunticon loader code snippet filename
-	loadersnippet: "grunticon.loader.js",
+  // grunticon loader code snippet filename
+  loadersnippet: "grunticon.loader.js",
 
-	// Include loader code for SVG markup embedding
-	enhanceSVG: true,
+  // Include loader code for SVG markup embedding
+  enhanceSVG: true,
 
-	// Make markup embedding work across domains (if CSS hosted externally)
-	corsEmbed: false,
+  // Make markup embedding work across domains (if CSS hosted externally)
+  corsEmbed: false,
 
-	// folder name (within dest) for png output
-	pngfolder: "png",
+  // folder name (within dest) for png output
+  pngfolder: "png",
 
-	// prefix for CSS classnames
-	cssprefix: ".icon-",
+  // prefix for CSS classnames
+  cssprefix: ".icon-",
 
-	defaultWidth: "300px",
-	defaultHeight: "200px",
+  defaultWidth: "300px",
+  defaultHeight: "200px",
 
-	// define vars that can be used in filenames if desirable,
+  // define vars that can be used in filenames if desirable,
   // like foo.colors-primary-secondary.svg
-	colors: {
-		primary: "red",
-		secondary: "#666"
-	},
+  colors: {
+    primary: "red",
+    secondary: "#666"
+  },
 
-	dynamicColorOnly: true,
+  dynamicColorOnly: true,
 
-	// css file path prefix
+  // css file path prefix
   // this defaults to "/" and will be placed before the "dest" path
   // when stylesheets are loaded. It allows root-relative referencing
   // of the CSS. If you don't want a prefix path, set to to ""
-	cssbasepath: "/",
-	customselectors: {
-		"cat" : ["#el-gato"],
-		"gummy-bears-2" : ["nav li a.deadly-bears:before"]
-	},
+  cssbasepath: "/",
+  customselectors: {
+    "cat" : ["#el-gato"],
+    "gummy-bears-2" : ["nav li a.deadly-bears:before"]
+  },
 
-	template: path.join( __dirname, "default-css.hbs" ),
-	previewTemplate: path.join( __dirname, "preview-custom.hbs" ),
+  template: path.join( __dirname, "default-css.hbs" ),
+  previewTemplate: path.join( __dirname, "preview-custom.hbs" ),
 
-	compressPNG: true
+  compressPNG: true
 };
--- tasks/gulpicon.js
+++ tasks/gulpicon.js
@@ -3,39 +3,39 @@ var gutil = require('gulp-util');
 var Grunticon = require( 'grunticon-lib' );
 
 module.exports = function( files, config ) {
-	"use strict";
+  "use strict";
 
   return function(callback) {
 
-	  // get the config
-	  config.logger = {
-			verbose: function() {} || config.verbose,
-			fatal: function() {},
-			ok: function() {}
-	  };
+    // get the config
+    config.logger = {
+      verbose: function() {} || config.verbose,
+      fatal: function() {},
+      ok: function() {}
+    };
 
-	  // just a quick starting message
-	  gutil.log( "Look, it's a gulpicon!" );
+    // just a quick starting message
+    gutil.log( "Look, it's a gulpicon!" );
 
-	  files = files.filter( function( file ){
-		  return file.match( /png|svg/ );
-	  });
+    files = files.filter( function( file ){
+      return file.match( /png|svg/ );
+    });
 
-	  if( files.length === 0 ){
-		  gutil.log( "Grunticon has no files to read!" );
-		  callback( false );
-		  return;
-	  }
+    if( files.length === 0 ){
+      gutil.log( "Grunticon has no files to read!" );
+      callback( false );
+      return;
+    }
 
-	  var output = config.dest;
+    var output = config.dest;
 
-	  if( !output || output && output === "" ){
-		  gutil.log("The destination must be a directory");
-		  callback( false );
-	  }
+    if( !output || output && output === "" ){
+      gutil.log("The destination must be a directory");
+      callback( false );
+    }
 
-	  var gicon = new Grunticon(files, config.dest, config);
+    var gicon = new Grunticon(files, config.dest, config);
 
-	  gicon.process( callback );
+    gicon.process( callback );
   };
 };
